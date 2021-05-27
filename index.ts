55c08b01bd069a3a7837971454c56daaf73e0d5b
Wasiu Idowu <hoxtygen@live.com>
2021-05-27 13:05:51 +0100

    Added start script to package.json file and installed helment and cors packages

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
-import express from "express";
+import express, {Application, Request, Response}  from "express";
 import dotenv from "dotenv"
+import helmet from "helmet"
+import cors from "cors";
 dotenv.config();
 
-const app = express();
+const app:Application = express();
+
+app.use(helmet());
+app.use(cors());
+
+
+
+
 const port = process.env.PORT || 4001;
-app.get("/", (req: Request, res) => {
+app.get("/", (req: Request, res:Response) => {
     res.status(200).json({
         message: "Welcome to news blog"
     });
 });
 
-app.listen(port, err => {
+app.listen(port, (err: any) => {
     if (err) {
         return console.error(err);
     }
